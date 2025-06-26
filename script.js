589c93059f64c37dcda648d4f69c34fb2fde0cd9
Stayna <staynaa.cs@gmail.com>
2025-06-25 21:49:44 -0400

    HTML/CSS implemented
    html file and css files created and completed
    script file created and added const values of html objects

---
+++ script.js
@@ -0,0 +1,12 @@
+const searchBar = document.getElementById("text-box");
+const fruitNameBox = document.getElementById("notif-text-box");
+const calAmtText = document.getElementById("carlorie-amt");
+const fatGramText = document.getElementById("fat-grams");
+const choGramText = document.getElementById("cho-grams");
+const sodGramText = document.getElementById("sod-grams");
+const carbGramText = document.getElementById("carb-grams");
+const sugarGramText = document.getElementById("sugar-grams");
+const proGramText = document.getElementById("pro-grams");
+
+const familyText = document.getElementById("family-text");
+const genusText = document.getElementById("genus-text");
